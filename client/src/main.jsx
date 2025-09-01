85dbe556f29579a9923aefcfd4ae524baa6e6a9b
Eyejangg <ptwptw1550@gmail.com>
2025-09-01 15:50:36 +0700

    main-auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import router from "./routes/Router";
 import { RouterProvider } from "react-router";
 import Navbar from "./components/Navbar";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
